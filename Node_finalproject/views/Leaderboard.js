3a55980c36e45fa8eb82096b1fd6f7a34bacc590
Ryan Eakin <Eakinryan@hotmail.com>
2019-04-06 23:10:32 -0700

    Changes to Leaderboard.hbs/ creation of Leaderboard.js AJAX on Leaderboard.js nearly done!

--- Node_finalproject/views/Leaderboard.js
+++ Node_finalproject/views/Leaderboard.js
@@ -1,3 +1,5 @@
+unames = '';
+uscore = '';
 var xhr = new window.XMLHttpRequest(),
     method = "get",
     url = "http://localhost:8080/getscores";
@@ -13,7 +15,15 @@ getscores = xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200){
         //console.log(this.response);
         scores = JSON.parse(this.response);
-        console.log(scores[0].Username)
+
+
+        for (var score = 0; score <= 1; score++){
+            unames += scores[score].Username+"\n";
+            uscore += scores[score].totalClicks+"\n";
+        }
+        document.getElementById('names').innerText = unames;
+        document.getElementById('userscores').innerText = uscore;
+
         }
 };
 
